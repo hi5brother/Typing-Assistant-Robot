238f8cb40eeb36036da4116faf04ee9d73bc519e
Daniel <daniel.kao.95@gmail.com>
2014-03-16 12:52:52 -0400

    im getting there...

--- two-line-follow.c
+++ two-line-follow.c
@@ -21,8 +21,108 @@ after first occurrence, the robot will rotate by __ degrees each time it senses 
 
 #define MAX 10
 
+
+
+typedef int range[11];
+
 int zigrotate (int speed, int degrees, int lightTarget);
-void followLine(int direction, int &bump);
+void drive;
+void rotate(int speed, int degrees,int zigged);
+int scanLight(int lightTarget,range ColourRange);
+void generateRange(int lightTarget, range colourRange);
+
+task main(){
+
+
+}
+
+void generateRange(int lightTarget, range colourRange){
+	int i;
+	
+	for (i=0;i<=11;i++)
+	{
+		colourRange[i]=lightTarget-MAX/2+i;	//sets the colour range to +/- 5 of the target light
+	}
+}
+
+int scanLight(int lightTarget,range colourRange){		//this will be used every second to scan if it is on a colour
+	int light;
+	int hit=0;
+	
+	SensorType[LIGHT]=sensorLightActive;
+	
+	light=SensorValue[LIGHT];
+	
+	for (i=0;i<=11;i++){
+		if (light==colourRange[i]){
+			hit=1;
+			break;
+		}
+	}
+	if (hit==1)
+		return 1;		//returns 1 if it is on the colour
+	else if (hit==0)
+		return 0;
+}
+
+void rotate (int speed, int degrees){
+	int neededCount, actualCount;
+    
+    if (degrees>0){
+        nMotorEncoder[RMOTOR]=0; //zero right encoder
+        motor[RMOTOR]=SPEED;
+        motor[LMOTOR]=-SPEED;
+    }
+    else{
+        nMotorEncoder[LMOTOR]=0; //zero left encoder
+        motor[RMOTOR]=-SPEED;
+        motor[LMOTOR]=SPEED;
+    }
+    
+    neededCount=abs(degrees)*ROTATIONSCALE;
+    actualCount=0;
+    
+    while(actualCount<(neededCount-SPEED*EARLYSTOPFACTOR)){
+        wait1MSec(5);
+        
+        if (degrees>0)
+            actualCount=nMotorEncoder[RMOTOR];
+        else
+            actualCount=nMotorEncoder[LMOTOR];
+        }
+    
+    motor[RMOTOR]=0;
+    motor[LMOTOR]=0;
+    wait1Msec(200);
+
+}
+
+void drive (int lightTarget, range colourRange){
+	int i;
+	int zigged=1;
+	nMotorPIDSpeedCtrl[LMOTOR]=mtrSpeedReg;
+	nMotorPIDSpeedCtrl[RMOTOR]=mtrSpeedReg;
+	
+	motor[RMOTOR]=SPEED;
+	motor[LMOTOR]=SPEED;
+	
+	while (scanLight(lightTarget, colourRange)==0){
+		motor[RMOTOR]=SPEED;
+		motor[LMOTOR]=SPEED;
+	}
+	if (scanLight(lightTarget,colourRange)==1){
+		zigged=zigged*(-1);
+		switch (zigged){
+			case 1:
+				rotate(20,20);
+				break;
+			case -1:
+				rotate(20,20);
+				break;
+		}
+			
+	}
+}
 
 
 
@@ -59,15 +159,7 @@ int zigrotate (int speed, int degrees, int lightTarget){
 		for (i=0;i<=MAX;i++)
 		{
 			if (light==colourRange[i])
-			{
-				onTarget=1;
-				nxtDisplayTextLine(7,"BREAK: %d",colourRange[i]);
-				degrees=0;
-				neededCount=abs(degrees)*ROTATESCALE;
-				return 1;
-			}
-		}
-		
+			 
 		wait1Msec(1);
 		
 		if (neededCount>0)
